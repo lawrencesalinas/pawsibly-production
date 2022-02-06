4dc3c86279db77c1c0a390c54f231ff79607cfdd
Lawrence <lawrencesalinas@yahoo.com>
2022-02-06 17:11:37 -0500

    updated styling and ckeaned code for DRY code

--- pawsibly/settings.py
+++ pawsibly/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'Pawsibly-react-frontend/build' )
+            os.path.join(BASE_DIR, 'build' )
         ],
         'APP_DIRS': True,
         'OPTIONS': {
