9d3bbafa96f8fe3a4833b46f1e3a298393786bfa
Lawrence <lawrencesalinas@yahoo.com>
2024-01-16 10:43:41 -0500

    updated settings to be deployed to render

--- pawsibly/settings.py
+++ pawsibly/settings.py
@@ -58,8 +58,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pawsibly', 
         'USER': 'lawrencesalinas',
-        'PASSWORD': 'ATommyBenny1002$S',
-        'HOST': 'pawsibly-identifier.c3o06us00092.us-east-2.rds.amazonaws.com',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '5432'
     }
 }
@@ -239,9 +239,9 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 AWS_STORAGE_BUCKET_NAME = 'pawsibly-bucket'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_CUSTOM_DOMAIN = f'pawsibly-bucket.s3.amazonaws.com'
-AWS_SECRET_ACCESS_KEY = 'oFEQ6+g8WtDTa67xHbtsB7m+jLC0QENDwerVueYk'
-AWS_ACCESS_KEY_ID = 'AKIA56I3WPRSPDLNNY2Q'
+AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 
 
 # get current working directory false if we are in heroku
